9fe2a13837af6922bd31d0af9cc84c0879ad0718
SafiAhmed <safianjum@addo.ai>
2021-08-27 12:21:20 +0500

    updated

--- jobs/pyspark_example.py
+++ jobs/pyspark_example.py
@@ -8,14 +8,23 @@ spark = SparkSession \
     .getOrCreate()
 
 # Loading data into dataframe
-trnasactions = spark.sql("SELECT * FROM literacyrate")
+literacyrate = spark.sql("SELECT * FROM bda_lab.literacyrate")
+
+# Creating temp view
+literacyrate.createOrReplaceTempView("literacyrate_tmp_tbl")
 
 # Transforming data
-#df_transformed = spark.sql(" ")
+df_transformed = spark.sql("""
+                    SELECT
+                        SUM(males) as total_males,
+                        SUM(females) as total_female
+                    FROM literacyrate_tmp_tbl
+                    GROUP BY districts, literacyratemales, literacyratefemales
+                """)
 
 # Displaying results
 df_transformed.show(10, truncate=0)
 
 # Getting count
 #rec_count = df_transformed.count()
-#print("Total Records: {}").format(rec_count)
+#print("Total Records: {0}").format(rec_count)
