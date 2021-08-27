e6b062c7e859b76a3a8936a11f7f50831fa9be66
SafiAhmed <safianjum@addo.ai>
2021-08-27 12:37:34 +0500

    updated

--- jobs/pyspark_example.py
+++ jobs/pyspark_example.py
@@ -4,27 +4,28 @@ from pyspark.sql import SparkSession
 # Building Spark Session (Function)
 spark = SparkSession \
     .builder \
-    .appName('PySpark-Local-App') \
+    .appName('TRANSACTIONS_BY_MONTH App') \
     .getOrCreate()
 
 # Loading data into dataframe
-literacyrate = spark.sql("SELECT * FROM bda_lab.literacyrate")
+transactions_by_month = spark.sql("SELECT * FROM bda_lab.transactions_by_month")
+
+# Display the loaded data
+transactions_by_month.show(10, truncate=0)
 
 # Creating temp view
-literacyrate.createOrReplaceTempView("literacyrate_tmp_tbl")
+transactions_by_month.createOrReplaceTempView("transactions_by_month_tmp_tbl")
 
 # Transforming data
 df_transformed = spark.sql("""
-                    SELECT
-                        SUM(males) as total_males,
-                        SUM(females) as total_female
-                    FROM literacyrate_tmp_tbl
-                    GROUP BY districts, literacyratemales, literacyratefemales
-                """)
+    SELECT 
+        item_type, 
+        month, 
+        sum(quantity) as total_products_sold 
+    FROM transactions_by_month_tmp_tbl 
+    GROUP BY item_type, month
+""")
 
 # Displaying results
-df_transformed.show(10, truncate=0)
+df_transformed.show(40, truncate=0)
 
-# Getting count
-#rec_count = df_transformed.count()
-#print("Total Records: {0}").format(rec_count)
