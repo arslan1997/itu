2469bd62d2bf89fadf6f15000ac45c97ae156a22
SafiAhmed <safianjum@addo.ai>
2021-08-27 12:42:42 +0500

    updated

--- jobs/pyspark_example.py
+++ jobs/pyspark_example.py
@@ -10,9 +10,6 @@ spark = SparkSession \
 # Loading data into dataframe
 transactions_by_month = spark.sql("SELECT * FROM bda_lab.transactions_by_month")
 
-# Display the loaded data
-transactions_by_month.show(10, truncate=0)
-
 # Creating temp view
 transactions_by_month.createOrReplaceTempView("transactions_by_month_tmp_tbl")
 
@@ -21,12 +18,16 @@ df_transformed = spark.sql("""
     SELECT 
         item_type, 
         month, 
-        sum(quantity) as total_products_sold 
+        sum(quantity) as total_products_sold,
+        sum(total_price) as total_revenue 
     FROM transactions_by_month_tmp_tbl 
     GROUP BY item_type, month
     ORDER BY item_type
 """)
 
-# Displaying results
-df_transformed.show(40, truncate=0)
+# Display Original Loaded Data
+transactions_by_month.show(20, truncate=0)
+
+# Displaying results after transformation
+df_transformed.show(30, truncate=0)
 
